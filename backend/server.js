a2d1cb6d9c8455b0d4bd9d0c979e90747e2c4eed
Adeyemi juwon <samju7778@gmail.com>
2021-10-09 09:57:41 +0100

    another way

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(express.json());
 app.use('/api/products', productRoutes);
 const PORT = process.env.PORT || 5000;
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('../frontend/build'));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../frontend/build'));
+}
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
